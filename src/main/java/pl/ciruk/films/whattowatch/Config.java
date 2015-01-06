d73834afadc47259a7bb77b8d6f278f889cbce10
cpiotr <c.piotre@gmail.com>
2015-01-06 22:59:10 +0100

    Removing obsolete classes.

--- src/main/java/pl/ciruk/films/whattowatch/Config.java
+++
@@ -1,8 +0,0 @@
-package pl.ciruk.films.whattowatch;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class Config extends Application {
-}
