c1a4e6d4cd381bf2aaec557cb01091750a358a34
c.piotre <c.piotre@gmail.com>
2015-07-19 00:06:51 +0200

    Adding async operation to DescriptionProviders.

--- src/main/java/pl/ciruk/whattowatch/description/DescriptionProvider.java
+++ src/main/java/pl/ciruk/whattowatch/description/DescriptionProvider.java
@@ -3,7 +3,14 @@ package pl.ciruk.whattowatch.description;
 import pl.ciruk.whattowatch.title.Title;
 
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 public interface DescriptionProvider {
 	Optional<Description> descriptionOf(Title title);
+
+	default CompletableFuture<Optional<Description>> descriptionOfAsync(Title title) {
+		return CompletableFuture.supplyAsync(
+				() -> descriptionOf(title)
+		);
+	}
 }
