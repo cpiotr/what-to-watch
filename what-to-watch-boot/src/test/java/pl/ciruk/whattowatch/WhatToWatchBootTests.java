e27793af3be3169d65ed076487be2524005f3131
c.piotre <c.piotre@gmail.com>
2016-01-31 23:04:28 +0100

    Fix context test

--- what-to-watch-boot/src/test/java/pl/ciruk/whattowatch/WhatToWatchBootTests.java
+++ what-to-watch-boot/src/test/java/pl/ciruk/whattowatch/WhatToWatchBootTests.java
@@ -1,38 +1,25 @@
 package pl.ciruk.whattowatch;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import pl.ciruk.whattowatch.config.Application;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = WhatToWatchBoot.class)
 @ContextConfiguration(classes = WhatToWatchBootTests.TestConfig.class)
+@TestPropertySource(locations="classpath:application-dev.properties")
 public class WhatToWatchBootTests {
 
-	@Ignore
 	@Test
 	public void contextLoads() {
 	}
 
-	static class TestConfig extends Application {
-		@Bean(name = "zalukaj-login-url")
-		String loginUrl() {
-			return "";
-		}
-
-		@Bean(name = "zalukaj-login")
-		String login() {
-			return "";
-		}
-
-		@Bean(name = "zalukaj-password")
-		String password() {
-			return "";
-		}
+	@ComponentScan("pl.ciruk")
+	static class TestConfig {
+		// Nothing to override
 	}
 }
