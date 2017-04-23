db7ba3b1b4801fbd6b0e46115126c2a1295c2c24
c.piotre <c.piotre@gmail.com>
2017-04-23 23:52:46 +0200

    Wrap circuit breaker around cache

--- what-to-watch-boot/src/main/java/pl/ciruk/whattowatch/cache/RedisCache.java
+++ what-to-watch-boot/src/main/java/pl/ciruk/whattowatch/cache/RedisCache.java
@@ -1,5 +1,6 @@
 package pl.ciruk.whattowatch.cache;
 
+import com.netflix.hystrix.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import pl.ciruk.core.cache.CacheProvider;
@@ -30,7 +31,7 @@ public class RedisCache implements CacheProvider<String> {
 
     @Override
     public Optional<String> get(String key) {
-        Optional<String> optional = Optional.ofNullable(cache.opsForValue().get(key));
+        Optional<String> optional = createGetCommand(key).execute();
         if (optional.isPresent()) {
             hitCounter.incrementAndGet();
         }
@@ -39,7 +40,37 @@ public class RedisCache implements CacheProvider<String> {
 
     @Override
     public void put(String key, String value) {
-        cache.opsForValue().set(key, value);
+        createPutCommand(key, value).execute();
+    }
+
+    private HystrixCommand<Optional<String>> createGetCommand(String key) {
+        return new HystrixCommand<Optional<String>>(() -> "get") {
+            @Override
+            protected Optional<String> run() throws Exception {
+                return Optional.ofNullable(
+                        cache.opsForValue().get(key));
+            }
+
+            @Override
+            protected Optional<String> getFallback() {
+                return Optional.empty();
+            }
+        };
+    }
+
+    private HystrixCommand<Optional<Void>> createPutCommand(String key, String value) {
+        return new HystrixCommand<Optional<Void>>(() -> "put") {
+            @Override
+            protected Optional<Void> run() throws Exception {
+                cache.opsForValue().set(key, value);
+                return Optional.empty();
+            }
+
+            @Override
+            protected Optional<Void> getFallback() {
+                return Optional.empty();
+            }
+        };
     }
 }
 
