a4a6f5f45dd46c13f67b99a3a3e3e223243bbd88
c.piotre <c.piotre@gmail.com>
2017-06-09 23:08:04 +0200

    Filter empty IMDB selected elements

--- what-to-watch-core/src/main/java/pl/ciruk/whattowatch/score/imdb/ImdbSelectors.java
+++ what-to-watch-core/src/main/java/pl/ciruk/whattowatch/score/imdb/ImdbSelectors.java
@@ -9,11 +9,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
+import static pl.ciruk.core.stream.Predicates.not;
+
 public enum ImdbSelectors implements Extractable<Optional<String>> {
     YEAR(result -> result.select(".lister-item-content .lister-item-header .lister-item-year")
             .stream()
             .map(Element::text)
             .map(text -> text.replaceAll("[^0-9]", ""))
+            .filter(not(String::isEmpty))
             .findFirst()),
     TITLE(result -> result.select(".lister-item-content .lister-item-header a")
             .stream()
@@ -36,11 +39,13 @@ public enum ImdbSelectors implements Extractable<Optional<String>> {
             .stream()
             .filter(div -> div.hasAttr("data-value"))
             .map(div -> div.attr("data-value"))
+            .filter(not(String::isEmpty))
             .findFirst()),
     NUMBER_OF_SCORES(result -> result.select(".sort-num_votes-visible span")
             .stream()
             .filter(span -> span.hasAttr("data-value"))
             .map(span -> span.attr("data-value"))
+            .filter(not(String::isEmpty))
             .findFirst()),;
 
     private Function<Element, Optional<String>> extractor;
