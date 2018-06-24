0935576d4750c9021b4e3bc127e2d33e5a00b706
c.piotre <c.piotre@gmail.com>
2018-06-24 18:47:01 +0200

    Mdularize core

---
+++ what-to-watch-core/src/main/java/module-info.java
@@ -0,0 +1,22 @@
+module what.to.watch.core {
+    exports pl.ciruk.whattowatch.core.suggest;
+    exports pl.ciruk.whattowatch.utils.concurrent;
+    exports pl.ciruk.whattowatch.core.title;
+    exports pl.ciruk.whattowatch.utils.net;
+    exports pl.ciruk.whattowatch.core.title.ekino;
+    exports pl.ciruk.whattowatch.core.source;
+    exports pl.ciruk.whattowatch.core.description.filmweb;
+    exports pl.ciruk.whattowatch.core.description;
+    exports pl.ciruk.whattowatch.core.score;
+    exports pl.ciruk.whattowatch.core.score.imdb;
+    exports pl.ciruk.whattowatch.core.score.filmweb;
+    exports pl.ciruk.whattowatch.core.score.metacritic;
+    exports pl.ciruk.whattowatch.utils.cache;
+    exports pl.ciruk.whattowatch.utils.stream;
+    requires jsoup;
+    requires org.slf4j;
+    requires micrometer.core;
+    requires okhttp3;
+    requires com.google.common;
+    requires com.fasterxml.jackson.databind;
+}
