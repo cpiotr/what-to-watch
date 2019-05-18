2bf4cc5836062b38f59d35e9eb75ef9712b33b10
c.piotre <c.piotre@gmail.com>
2019-05-18 20:44:11 +0200

    Extract in-memory cookie storage

---
+++ what-to-watch-core/src/main/java/pl/ciruk/whattowatch/utils/net/InMemoryCookieJar.java
@@ -0,0 +1,25 @@
+package pl.ciruk.whattowatch.utils.net;
+
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.HttpUrl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+class InMemoryCookieJar implements CookieJar {
+    private final Map<String, List<Cookie>> cookiesByHost = new ConcurrentHashMap<>();
+
+    @Override
+    public void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
+        List<Cookie> existingCookies = cookiesByHost.computeIfAbsent(url.host(), __ -> new ArrayList<>());
+        existingCookies.addAll(cookies);
+    }
+
+    @Override
+    public List<Cookie> loadForRequest(HttpUrl url) {
+        return cookiesByHost.getOrDefault(url.host(), new ArrayList<>());
+    }
+}
