39c693e2055df8214e3f1ea8b2554781ec4d324d
c.piotre <c.piotre@gmail.com>
2020-06-12 21:46:39 +0200

    Remove unused exception class

--- what-to-watch-core/src/main/java/pl/ciruk/whattowatch/utils/concurrent/AsyncExecutionException.java
+++
@@ -1,7 +0,0 @@
-package pl.ciruk.whattowatch.utils.concurrent;
-
-public class AsyncExecutionException extends RuntimeException {
-    public AsyncExecutionException(Throwable throwable) {
-        super(throwable);
-    }
-}
