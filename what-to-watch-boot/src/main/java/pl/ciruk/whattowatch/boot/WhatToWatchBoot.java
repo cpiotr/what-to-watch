8536a24cca61523329127b6d236c18a90112c8b5
c.piotre <c.piotre@gmail.com>
2023-06-19 23:58:01 +0200

    Hint local URL when application ready

--- what-to-watch-boot/src/main/java/pl/ciruk/whattowatch/boot/WhatToWatchBoot.java
+++ what-to-watch-boot/src/main/java/pl/ciruk/whattowatch/boot/WhatToWatchBoot.java
@@ -1,12 +1,30 @@
 package pl.ciruk.whattowatch.boot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+
+import java.lang.invoke.MethodHandles;
 
 @SpringBootApplication
 @SuppressWarnings("PMD")
-public class WhatToWatchBoot {
+public class WhatToWatchBoot implements ApplicationListener<ApplicationReadyEvent> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Value("${server.port:8080}")
+    Integer serverPort;
+
     public static void main(String[] args) {
         SpringApplication.run(WhatToWatchBoot.class, args);
+
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        LOGGER.info("Open the application: http://{}:{}/events.html", "localhost", serverPort);
     }
 }
